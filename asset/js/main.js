c09876a995c90f47911cfb3a694bda161e54d1ad
Example113355 <tson113355@gmail.com>
2023-11-18 23:34:20 +0700

    add page 2 and 3

--- asset/js/main.js
+++ asset/js/main.js
@@ -3,7 +3,7 @@ const userInfo = document.querySelector(".user_info")
 const content = document.querySelector(".content")
 
 function showSubInfo() {
-  subInfo.classList.add("show")
+  subInfo.classList.toggle("show")
 }
 
 function hideSubInfo() {
@@ -12,3 +12,24 @@ function hideSubInfo() {
 
 userInfo.addEventListener("click", showSubInfo)
 content.addEventListener("click", hideSubInfo)
+
+function incCurPage() {
+  const max = document.querySelector(".total").innerHTML
+  const input = document.querySelector(".current_page input")
+
+  var curValue = input.attributes.value.value
+  var nextValue = parseInt(curValue) + 1
+  if(nextValue <= parseInt(max)) {
+    input.attributes.value.value = nextValue
+  }
+}
+
+function decCurPage() {
+  const input = document.querySelector(".current_page input")
+
+  var curValue = input.attributes.value.value
+  var nextValue = parseInt(curValue) - 1
+  if(nextValue > 0) {
+    input.attributes.value.value = nextValue
+  }
+}
